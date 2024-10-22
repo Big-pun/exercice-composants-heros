ff236de2145863fade791b94da8da6e4cabc6296
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-10-22 19:46:12 -0400

    avant liste

--- src/app/entete/entete.component.ts
+++ src/app/entete/entete.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-entete',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './entete.component.html',
   styleUrl: './entete.component.css'
 })
--- src/app/recherche/recherche.component.ts
+++ src/app/recherche/recherche.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-recherche',
   standalone: true,
-  imports: [],
+  imports: [ MatSidenavModule, MatButtonModule ],
   templateUrl: './recherche.component.html',
   styleUrl: './recherche.component.css'
 })
 export class RechercheComponent {
-
+ showFiller = false;
 }
